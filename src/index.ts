e1d09315bbba602960a1d906f6dad4095d62c86a
James Penn <james@jamespenn.co.uk>
2022-06-27 12:36:38 +0100

    run linters

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,13 @@ import { Widget } from '@lumino/widgets';
 import { requestAPI } from './handler';
 
 function formatRemaining(timedelta: number): string {
-  const td = new Date(Date.UTC(0, 0, 0, 0, 0, 0, timedelta))
+  const td = new Date(Date.UTC(0, 0, 0, 0, 0, 0, timedelta));
   return td.toLocaleTimeString([], {
-    hour: '2-digit', minute: '2-digit', second: '2-digit'
-  })
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  });
 }
-
 
 /**
  * Initialization data for the slurm-time extension.
@@ -23,7 +24,6 @@ const plugin: JupyterFrontEndPlugin<void> = {
   autoStart: true,
   requires: [IStatusBar],
   activate: (app: JupyterFrontEnd, statusBar: IStatusBar) => {
-
     console.log('JupyterLab extension slurm-time activated');
 
     requestAPI<any>('get_time')
@@ -31,30 +31,30 @@ const plugin: JupyterFrontEndPlugin<void> = {
         // TODO: check data.data.error and fail if errored
 
         const endTime = new Date();
-        endTime.setSeconds(endTime.getSeconds() + data.data.remaining)
+        endTime.setSeconds(endTime.getSeconds() + data.data.remaining);
 
         const statusWidget = new Widget();
 
         setInterval(() => {
-          let now = new Date()
-          let delta = endTime.getTime() - now.getTime()
-          statusWidget.node.textContent = `Time remaining: ${formatRemaining(delta)}`
-        }, 1000)
+          const now = new Date();
+          const delta = endTime.getTime() - now.getTime();
+          statusWidget.node.textContent = `Time remaining: ${formatRemaining(
+            delta
+          )}`;
+        }, 1000);
 
         statusBar.registerStatusItem('lab-status', {
           align: 'middle',
           item: statusWidget
         });
 
-        console.log(`Session will end at ${endTime}`)
+        console.log(`Session will end at ${endTime}`);
       })
       .catch(reason => {
         console.error(
           `The slurm_time server extension appears to be missing.\n${reason}`
         );
       });
-
-
   }
 };
 
